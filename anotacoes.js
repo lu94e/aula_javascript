d4c89def450547dcfdfd55a0790e9292903a673b
lu94e <lucas_ss94@hotmail.com>
2022-01-17 14:19:54 -0300

    Constantes, Variaveis e funçoes

---
+++ anotacoes.js
@@ -0,0 +1,14 @@
+/* Diferençã entre variável e constante.
+A variável pode ser manipulada e modificada, já a constante não. */
+
+const PRECO = 2;
+var desconto = 0.2;
+var valor_total = PRECO - desconto;
+
+// Funções
+// Estrutura de uma função
+function soma(a, b) {
+    return a + b;
+}
+// Envocando uma função.
+soma(3, 5);
